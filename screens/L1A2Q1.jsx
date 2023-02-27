512e16ba013121617b9b517e1010b861486cb922
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-02-27 03:07:06 -0500

    Corrected L1A2

--- screens/L1A2Q1.jsx
+++ screens/L1A2Q1.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React,  { useState, useEffect } from 'react';
 import { playAudio, stopAudio } from './functions/playAudio';
-import { StyleSheet,Text, View, TextInput,Image, TouchableOpacity, Linking } from 'react-native';
+import { Modal, StyleSheet,Text, View, TextInput,Image, TouchableOpacity, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DraxProvider, DraxView, DraxList } from 'react-native-drax';
@@ -13,14 +13,55 @@ import audios from './soundsL1A2';
 
 const gestureRootViewStyle = { flex: 1 };
 let puntaje = 0;
-let currentButtonText = 'Verificar';
+let currentQuestionIndex = 0
+let respuesta_correcta;
+
+let answer_state = 0;
 
 const L1A2Q1 = ({navigation}) => {
   app = getApp(); 
   const db = getFirestore();
-  const [ currentQuestionIndex, setCurrentQuestionIndex ] = useState(0);
+  //const [ currentQuestionIndex, setCurrentQuestionIndex ] = useState(0);
   const [ questions, setQuestions ] = useState(null);
   const segundos = useCronometro();
+  const [porcentaje, setPorcentaje] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  /*--------------------------------------------------------------------------------------------  */
+  /*---------------------------------------- Modal -----------------------------------------  */
+  /*--------------------------------------------------------------------------------------------*/
+  const handleComprobarPress = () => {
+    stopAudio()
+    setPorcentaje(porcentaje+100/questions.length)
+    respuesta_correcta = verifyConcatenation(receivingItemList) 
+    if (respuesta_correcta) {
+      puntaje = puntaje + 100/questions.length;
+    }
+    setModalVisible(true);
+  };
+
+  
+  const handleContinuePress = () => {
+    setModalVisible(false)
+    if (currentQuestionIndex === questions.length-1) {
+        navigation.navigate("Result", {puntuation3: Math.round(puntaje), 
+                                      time_taken: segundos, 
+                                      unit:1,
+                                      lesson:1, 
+                                      activity:2,
+                                      subtitle:'Colores/Tullpukuna'});
+        puntaje = 0;
+        currentQuestionIndex = 0
+      } else{
+        //setCurrentQuestionIndex(currentQuestionIndex + 1);
+        currentQuestionIndex = currentQuestionIndex + 1
+        console.log(currentQuestionIndex)
+        InitialDraggableItemList = questions[currentQuestionIndex].options;
+        resetLists()
+      }
+    }
+
+  
+
 
   const DragUIComponent = ({ item, index }) => {
     return (
@@ -60,7 +101,7 @@ const L1A2Q1 = ({navigation}) => {
           let newReceivingItemList = [...receivingItemList];
           newReceivingItemList[index] = draggedItem;
           setReceivingItemList(newReceivingItemList);
-  
+          answer_state = 1;
           let newDragItemMiddleList = [...dragItemMiddleList];
           newDragItemMiddleList[draggedPayload] = item;
           setDragItemMiddleList(newDragItemMiddleList);
@@ -76,27 +117,29 @@ const L1A2Q1 = ({navigation}) => {
   const resetLists = () => {
     setReceivingItemList(FirstReceivingItemList);
     setDragItemMiddleList(InitialDraggableItemList);
-  }
-  const verifyConcatenation = (receivingItemList, targetString) => {
-    let concatenatedString = '';
-    receivingItemList.forEach(item => {
-      concatenatedString += item.text ;
-    });
-    return concatenatedString === targetString;
+    answer_state = 0
   }
 
-  const result = () => {
-  if (verifyConcatenation(receivingItemList,correct_answer)){
-      alert('Respuesta:\n'+'Correcto');
-      resetLists();
-      puntaje = puntaje + 100/3;
-      
-  } else {
-      alert('Respuesta:\n'+'Incorrecto');
-      resetLists();
-      puntaje = puntaje
-  }};
-  let statement, InitialDraggableItemList, correct_answer,retrieved_audio;
+
+const verifyConcatenation = (receivingItemList) => {
+  let concatenatedString = '';
+  let itemCount = 0;
+  receivingItemList.forEach(item => {
+    concatenatedString += item.text;
+    itemCount++;
+
+    if (itemCount < receivingItemList.length) {
+      concatenatedString += ' ';
+    }
+  });
+
+  return concatenatedString === questions[currentQuestionIndex].correct_answer;
+}
+
+
+
+
+  let statement, InitialDraggableItemList, retrieved_audio;
   let FirstReceivingItemList = [
     {
       "id": 4,
@@ -137,38 +180,40 @@ const L1A2Q1 = ({navigation}) => {
         <Text>Loading...</Text>
       </View>
     );
-  } else{
+  } else {
     statement = questions[currentQuestionIndex].statement;
     InitialDraggableItemList = questions[currentQuestionIndex].options;
     correct_answer = questions[currentQuestionIndex].correct_answer;
     retrieved_audio = questions[currentQuestionIndex].audio;
+    
   }
   
   return (
-    <View style={styles.container}>
+    <View style={styles.AppContainer}>
 
-      <View style={{ margin: 20 }}>
-          <Text style={styles.Title}> Actividad2/Rurana 2</Text>
-        </View>
+      <View>
+          <Text style={styles.statementText}> Traduce la oracion </Text>
+      </View>
 
-        <View>
-        <Text style={styles.subTitle}> Traduce la oracion </Text>
-        </View>
+      <ProgressBar progress={porcentaje/100} width={300} 
+                   height={25} color={'#89D630'} unfilledColor={'#C8C8C8'}
+                   borderWidth={0} style= {{borderRadius:25}}
+                    />
         
-        <View style={{ flexDirection: 'row',margin: 60}}>
+      <View style={{ flexDirection: 'row',margin: 60}}>
           <Text style={styles.instructionText}> {statement}</Text>
           <TouchableOpacity onPress={() => {
             let audioPath = (audios.find((audio) => audio.name === retrieved_audio)).path
             playAudio(audioPath);
           }} >
-          <Icon name="volume-up" size={30} color="black"/>
+          <Icon name="volume-up" style = {{ paddingHorizontal:10 }}size={30} color="black"/>
           </TouchableOpacity>
           
-        </View>
+      </View>
           
       <GestureHandlerRootView style={gestureRootViewStyle}>
         <DraxProvider>
-          <View style={styles.container}>
+          <View style={styles.AppContainer}>
             <View style={styles.receivingContainer}>
               {receivingItemList.map((item, index) => ReceivingZoneUIComponent({ item, index }))}
             </View>
@@ -189,38 +234,41 @@ const L1A2Q1 = ({navigation}) => {
 
 
         <TouchableOpacity style={styles.buttonContainer} onPress={resetLists} >
-          <Text style={styles.buttonText}> Reset </Text> 
+          <Text style={styles.buttonText}> Reinciar </Text> 
         </TouchableOpacity>
+      </View>
+
+
+
+      {/* Comprobar Button */}
+      <View style={{ flexDirection: 'row',margin: 30}} >
+          <TouchableOpacity
+            style={answer_state === 0 ? styles.comprobarButton_Disabled : styles.comprobarButton_Enabled }
+            disabled={answer_state === 0}
+            onPress={handleComprobarPress}
+          >
+            <Text style={styles.comprobarText}>Comprobar</Text>
+          </TouchableOpacity>
+      </View>
+
+       {/* Modal */}   
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <View style={styles.modalContainer}>
+          {/* <View style={styles.modalContent}> */}
+            <Text style={respuesta_correcta? styles.modalTextCorrecto: styles.modalTextIncorrecto}>
+            {respuesta_correcta ? "¡Excelente!" : "Incorrecto"}
+            </Text>
+
+            <Text style={{color: 'white', paddingVertical: 10, fontSize: 17, opacity: respuesta_correcta ? 0: 1}}>
+              <Text style={{color: '#86D332'}}>Respuesta correcta: </Text>
+              {!respuesta_correcta ? questions[currentQuestionIndex].correct_answer : ''}
+            </Text>
+
+            <TouchableOpacity onPress={handleContinuePress} style={respuesta_correcta? styles.continueButton_Correct: styles.continueButton_Incorrect}>
+              <Text style = {styles.continueText}>Continuar</Text>
+            </TouchableOpacity>
         </View>
-
-
-        <View style={{ flexDirection: 'row',margin: 30}}>
-
-        <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => {
-          if (currentButtonText === 'Verificar'){
-              result();
-              stopAudio()
-              /* puntaje = puntaje + 100/3; */
-              currentButtonText = 'Continuar';
-          }
-          else if (currentButtonText === 'Continuar'){
-            resetLists();
-            currentButtonText = 'Verificar'
-            if (currentQuestionIndex === questions.length-1) {
-              navigation.navigate("Result", {puntuation3: Math.round(puntaje), time_taken: segundos, lesson:1, subtitle:'Colores/Tullpukuna'});
-              puntaje = 0;
-            } else{
-            setCurrentQuestionIndex(currentQuestionIndex + 1);
-            resetLists();
-            }
-          }
-        }}
-      >
-        <Text style={styles.buttonText}>{currentButtonText}</Text>
-      </TouchableOpacity>
-        </View> 
+      </Modal>
         
       <StatusBar style="auto" />
     </View>
@@ -228,18 +276,12 @@ const L1A2Q1 = ({navigation}) => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F5F5F8',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   AppContainer: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'space-around',
     alignItems: 'center',
-    paddingTop:20,
+    marginTop:30,
     paddingLeft:5,
     paddingRight:5
   },
@@ -250,10 +292,12 @@ const styles = StyleSheet.create({
     translateX: 200,
     translateY: 10
   },
-  subTitle: {
-    fontSize: 25,
-    color: '#F2570A',
-    fontWeight: 'bold'
+  statementText: {
+    color: '#F18701',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop:20,
+    marginBottom:20
   },
   instructionText: {
     fontSize: 20,
@@ -283,7 +327,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
   buttonContainer: {
-    backgroundColor: "#82C0CC",
+    backgroundColor: "#666BCA",
+    radius:50,
     marginTop: 25,
     borderRadius: 10,
     paddingVertical: 10,
@@ -300,20 +345,9 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: "white",
     alignSelf: "center",
-    fontWeight: 'bold'
+    //fontWeight: 'bold'
   },
-  bottomBar: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-    height: 50,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    backgroundColor: '#292D3E',
-    paddingHorizontal: 20,
-  },
+
   icon: {
     width: 30,
     height: 30,
@@ -321,29 +355,29 @@ const styles = StyleSheet.create({
   receivingZone: {
     height: 60,//(Dimensions.get('window').width / 4) - 12,
     borderRadius: 10,
-    width: 60,//(Dimensions.get('window').width / 4) - 12,
+    width: 90,//(Dimensions.get('window').width / 4) - 12,
     justifyContent: 'center',
     alignItems: 'center',
     marginRight: 5
   },
   receiving: {
     borderColor: 'blue',
-    borderWidth: 2,
+    borderWidth: 0,
   },
   draggableBox: {
-    width: 60,//(Dimensions.get('window').width / 4) - 12,
+    width: 90,//(Dimensions.get('window').width / 4) - 12,
     height:60,// (Dimensions.get('window').width / 4) - 12,
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    marginRight: 5
+    marginRight: 5,
   },
   dragging: {
     opacity: 0.2,
   },
   hoverDragging: {
-    borderColor: 'magenta',
-    borderWidth: 2,
+    borderColor: '#2D5288',
+    borderWidth: 0,
   },
   receivingContainer: {
     flexDirection: 'row',
@@ -363,8 +397,94 @@ const styles = StyleSheet.create({
     margin: 70
   },
   textStyle: {
-    fontSize: 18
-  }
+    fontSize: 18,
+    //color:'white'
+  },
+
+   // ------  Comprobar Button -----
+   comprobarButton_Enabled: {
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#82C0CC",
+    borderRadius: 20,
+  },
+  comprobarButton_Disabled: {
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#C3C3C3",
+    borderRadius: 20,
+    //opacity: 0
+  },
+
+
+  comprobarText:{
+    color: '#fff',
+    fontSize: 20
+  },
+
+   /* ------- Modal --------- */
+   modalContainer: {
+    position: 'absolute',
+    width:"100%",
+    bottom: 0,
+    backgroundColor: '#383A45',
+    //borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    //margin: 40,
+    marginHorizontal: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+
+  /* ------ Texto Modal -------- */
+  modalTextCorrecto:{
+    color: '#86D332',
+    fontWeight:'bold',
+    fontSize: 20
+  },
+
+  modalTextIncorrecto:{
+    color:'#EE5655',
+    fontWeight:'bold',
+    fontSize: 20,
+    //marginBottom:5
+  },
+  
+  /* ------ Button Continue -------- */
+  continueButton_Correct:{
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#86D332",
+    borderRadius: 20},
+  
+
+
+  continueButton_Incorrect:{
+    width: 200,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#EE5655",
+    borderRadius: 20},
+
+  continueText:{
+      color: '#fff',
+      fontSize: 20
+    }
 })
 
   export default L1A2Q1;
