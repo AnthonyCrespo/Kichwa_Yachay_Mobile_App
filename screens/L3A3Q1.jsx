48c167dce952355cd26d9876e82851b7e935dc32
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-01 00:01:12 -0500

    Added L3A1Q1

--- screens/L3A3Q1.jsx
+++ screens/L3A3Q1.jsx
@@ -1,75 +1,194 @@
 import { StatusBar } from 'expo-status-bar';
 import React,  { useState } from 'react';
 import { StyleSheet, Button,Text, View, TextInput,Image, TouchableOpacity, Linking } from 'react-native';
-
+  
 const L3A3Q1= ({navigation}) => {
+
+    const [leftItems, setLeftItems] = useState(['Perro', 'Cuál', 'Es', 'Negro', 'Blanco', 'Gato']);
+    const [rightItems, setRightItems] = useState(['Allku','Maykan', 'Yana', 'Missi', 'Yurak', 'Kan']);
+    const [selectedLeft, setSelectedLeft] = useState(null);
+    const [selectedRight, setSelectedRight] = useState(null);
+    const [correctPairs, setCorrectPairs] = useState([]);
+    const [showResults, setShowResults] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [incorrectPairs, setIncorrectPairs] = useState([]);
+/*     const [selectedPair, setSelectedPair] = useState({ left: null, right: null });
+ */
+ /*    const checkPair = (left, right) => {
+      return correctPairs.some((pair) => pair.left === left && pair.right === right);
+    }; */
+  
+    const handlePress = (type, item) => {
+      if (type === 'left') {
+        setSelectedLeft(item);
+      } else {
+        setSelectedRight(item);
+      }
+      if (selectedLeft && type === 'right') {
+
+        if (selectedLeft === 'Perro' && item === 'Allku') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        } else if (selectedLeft === 'Cuál' && item === 'Maykan') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        } 
+        else if (selectedLeft === 'Es' && item === 'Kan') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        }
+        else if (selectedLeft === 'Gato' && item === 'Missi') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        }
+        else if (selectedLeft === 'Negro' && item === 'Yana') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        }
+        else if (selectedLeft === 'Blanco' && item === 'Yurak') {
+          setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+        }
+        else {
+          setIncorrectPairs([...incorrectPairs, { left: selectedLeft, right: item }]);
+        }
+        setSelectedLeft(null);
+        setSelectedRight(null);
+      }
+    };
+    
+    //--------------
+    let numCorrect = 0;
+    let numIncorrect = 0;
+    //--------------
+    const handleCheckAnswers = () => {
+      const correctAnswers = [
+                              { left: 'Perro', right: 'Allku' }, 
+                              { left: 'Cuál', right: 'Maykan' },
+                              { left: 'Es', right: 'Kan' }, 
+                              { left: 'Gato', right: 'Missi' }, 
+                              { left: 'Negro', right: 'Yana' },
+                              { left: 'Blanco', right: 'Yurak' }, 
+                            ];
+      let isCorrect = true;
+      correctAnswers.forEach(answer=> {
+        if (!correctPairs.some(pair => pair.left === answer.left && pair.right === answer.right)) {
+          isCorrect = false;
+          numIncorrect += 1;
+          incorrectPairs.push(answer);
+        } else{numCorrect += 1;}
+      });
+      setShowResults(true);
+      
+      
+        /* 'Resultados', */
+        isCorrect ? '¡Felicidades, todas tus respuestas son correctas!' : 'Una o más respuestas son incorrectas'
+        [
+          {
+/*             text: 'OK',
+ */            onPress: () => setShowResults(false)
+          }
+        ]
+    
+    };
+
+    const resetActivity = () => {
+      setLeftItems(['Perro', 'Cuál', 'Es', 'Negro', 'Blanco', 'Gato']);
+      setRightItems(['Allku','Maykan', 'Yana', 'Missi', 'Yurak', 'Kan']);
+      setSelectedLeft(null);
+      setSelectedRight(null);
+      setCorrectPairs([]);
+      setIncorrectPairs([]);
+      setShowResults(false);
+    };
+
+
     return (
-        <View style={styles.container}>
-            <View style={{ margin: 20 }}>
-             <Text style={styles.Title}> Actividad 3/Rurana 3</Text>
-            </View>
-            
-            <Text style={styles.subTitle}> Relacione las palabras </Text>
-
-            <View style={{ flexDirection: 'row', margin: 40 }}>
-  <View style={{ flex: 1 }}>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Perro</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Cuál</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Es</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Negro</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Flanco</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Gato</Text>
-    </TouchableOpacity>
-  </View>
-  <View style={{ flex: 1 }}>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Maykan</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Yana</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Missi</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Allku</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Yurak</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.buttonSolution}>
-      <Text style={{ fontWeight: 'bold', fontSize: 20 }}>Kan</Text>
-    </TouchableOpacity>
-  </View>
-</View>
-
-
-<TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate("Result", {puntuation3: Math.round(100), time_taken: 70, lesson:3, subtitle:' '})}> 
-     <Text style={styles.buttonText}> Continuar </Text> 
-</TouchableOpacity>
-
-<StatusBar style="auto" />
-</View>
+      <View style={styles.container}>
+        <View style={{ margin: 20 }}>
+          <Text style={styles.Title}>Actividad 3/Rurana 3</Text>
+        </View>
+  
+        <Text style={styles.subTitle}>Relacione las palabras</Text>
 
         
-    )
-}
+        <View style={{ flexDirection: 'row', margin: 40 }}>
 
-const styles = StyleSheet.create({
+          
+<View style={{ flex: 1, flexDirection: 'column', flexWrap: 'wrap' }}>
+  {leftItems.map((item, index) => {
+    const isCorrect = correctPairs.find((pair) => pair.left === item);
+    const isIncorrect = incorrectPairs.find((pair) => pair.left === item);
+     return (
+      <TouchableOpacity
+        key={`left_${index}`}
+        style={[
+          styles.buttonSolution,
+          isCorrect && styles.correctSolution,
+          isIncorrect && styles.incorrectSolution,
+      ]}
+        onPress={() => {handlePress('left', item);
+                      console.log(selectedLeft)
+          
+                  }}
+        disabled={ /* correctPairs.find((pair) => pair.left === item) || incorrectPairs.find((pair) => pair.right === item)  &&*/ selectedLeft !== null}
+      >
+        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item}</Text>
+      </TouchableOpacity>
+    );
+  })}
+</View>
+
+<View style={{ flex: 1, flexDirection: 'column', flexWrap: 'wrap' }}>
+  {rightItems.map((item, index) => {
+     const isCorrect = correctPairs.find((pair) => pair.right === item);
+     const isIncorrect = incorrectPairs.find((pair) => pair.right === item);
+     return (
+      <TouchableOpacity
+        key={`right_${index}`}
+        style={[
+          styles.buttonSolution,
+          isCorrect && styles.correctSolution,
+          isIncorrect && styles.incorrectSolution ,
+        ]}
+        onPress={() => {
+          handlePress('right', item);
+        }}
+        disabled={/* correctPairs.find((pair) => pair.right === item) && incorrectPairs.find((pair) => pair.left === item) && */ selectedRight !== null}
+      >
+        <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item}</Text>
+      </TouchableOpacity>
+    );
+  })}
+</View>
+
+  </View>
+
+        
+          <View>
+            <TouchableOpacity style={styles.buttonContainer} onPress={resetActivity} >
+              <Text style={styles.buttonText}> Reset </Text> 
+            </TouchableOpacity>
+            </View>
+
+
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.checkButton}
+                onPress={() => {
+                  handleCheckAnswers();
+                  alert(numCorrect + " respuestas correctas, " + numIncorrect + " respuestas incorrectas");
+                  resetActivity();
+                }}
+              >
+                <Text style={styles.buttonText}>Verificar</Text>
+              </TouchableOpacity>
+            </View>
+
+  
+        <StatusBar style="auto" />
+      </View>
+    );
+  };
+  
+  const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#F5F5F8',
+      backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
@@ -85,51 +204,38 @@ const styles = StyleSheet.create({
       color: '#F2570A',
       fontWeight: 'bold'
     },
-    instructionText: {
-      fontSize: 20,
-      color: 'black',
-      fontWeight: 'bold'
-    },
-  
-    buttonSolution: {
-      backgroundColor: "#B9B6B6",
-      marginTop: 25,
+    buttonContainer: {
+      backgroundColor: '#82C0CC',
+      padding: 10,
       borderRadius: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 9,
-      marginRight: 20,
+      marginTop: 30,
+    },
+    buttonText: {
+      color: 'black',
       fontWeight: 'bold',
       fontSize: 20,
+      textAlign: 'center',
     },
-    buttonContainer: {
-      backgroundColor: "#82C0CC",
-      marginTop: 25,
+    buttonSolution: {
+      backgroundColor: 'lightgray',
+      padding: 10,
       borderRadius: 10,
-      paddingVertical: 10,
-      paddingHorizontal: 50
-      },
-    buttonText: {
-      fontSize: 20,
-      color: "white",
-      alignSelf: "center",
-      fontWeight: 'bold'
-    },
-    bottomBar: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      bottom: 0,
-      height: 50,
-      flexDirection: 'row',
+      marginBottom: 10,
+      justifyContent: 'center',
       alignItems: 'center',
-      justifyContent: 'space-around',
-      backgroundColor: '#292D3E',
-      paddingHorizontal: 20,
+      borderWidth: 1,
+      borderColor: 'gray',
+      width: '50%',
+      height: 50,
     },
-    icon: {
-      width: 30,
-      height: 30,
+    correctSolution: {
+      backgroundColor: 'green',
     },
-  })
-
+    incorrectSolution: {
+      backgroundColor:'red',
+    }, 
+   /*  Answer: {
+      backgroundColor: '#48d1cc',
+    },  */
+  });
   export default L3A3Q1
