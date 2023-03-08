09b8888e5cfd59f4f9d7830b159d15d010986829
Mateo-Coello <91999743+Mateo-Coello@users.noreply.github.com>
2023-03-08 16:26:42 -0500

    Update imagesL2A1.jsx

--- screens/imagesL2A1.jsx
+++ screens/imagesL2A1.jsx
@@ -3,6 +3,9 @@ const images = [
   { name: 'yellow-dog.png', path: require('../assets/images/unit_1/lesson_2/activity_1/yellow-dog.png') },
   { name: 'yellow-serpent.png', path: require('../assets/images/unit_1/lesson_2/activity_1/yellow-serpent.png') },
   { name: 'white-cat.png', path: require('../assets/images/unit_1/lesson_2/activity_1/white-cat.png') },
-  ];
+  { name: 'red-fish.png', path: require('../assets/images/unit_1/lesson_2/activity_1/red-fish.png') },
+  { name: 'hen.png', path: require('../assets/images/unit_1/lesson_2/activity_1/hen.png') },
+  { name: 'pig.png', path: require('../assets/images/unit_1/lesson_2/activity_1/pig.png') },
+];
      
   export default images;
