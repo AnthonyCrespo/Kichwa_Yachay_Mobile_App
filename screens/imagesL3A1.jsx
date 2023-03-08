c8a063986adcbb260fb1a27ee3fc6c635611ab39
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-07 22:47:12 -0500

    Corrected L3A1 image

--- screens/imagesL3A1.jsx
+++ screens/imagesL3A1.jsx
@@ -7,7 +7,7 @@
 
   const images = [
     { name: 'black-cat.png', path: require('../assets/images/unit_1/lesson_3/activity_1/black-cat.png') },
-    { name: 'black-dog.png', path: require('../assets/images/unit_1/lesson_3/activity_1/yellow-serpent.png') },
+    { name: 'black-dog.png', path: require('../assets/images/unit_1/lesson_3/activity_1/black-dog.png') },
     { name: 'bull.png', path: require('../assets/images/unit_1/lesson_3/activity_1/bull.png') },
     { name: 'hen.png', path: require('../assets/images/unit_1/lesson_3/activity_1/hen.png') },
     { name: 'cow.png', path: require('../assets/images/unit_1/lesson_3/activity_1/cow.png') },
