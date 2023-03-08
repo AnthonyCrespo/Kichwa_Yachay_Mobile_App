66603f65fe02e3bfb59d6120f244badb7367ed3b
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-08 15:27:20 -0500

    added new sound L3A1

--- screens/soundsL3A1.jsx
+++ screens/soundsL3A1.jsx
@@ -7,6 +7,8 @@ const audios = [
     { name: 'allkuka_yurakmi_kan.wav', path: require('../assets/audios/unit_1/lesson_3/activity_1/allkuka_yurakmi_kan.wav') },
     { name: 'allkuka_yanami_kan.wav', path: require('../assets/audios/unit_1/lesson_3/activity_1/allkuka_yanami_kan.wav') },
     { name: 'wakraka_yurakmi_kan.wav', path: require('../assets/audios/unit_1/lesson_3/activity_1/wakraka_yurakmi_kan.wav') },
+    { name: 'atallpaka_yanami_kan.wav', path: require('../assets/audios/unit_1/lesson_3/activity_1/atallpaka_yanami_kan.wav') },
+    { name: 'challwaka_pukami_kan.wav', path: require('../assets/audios/unit_1/lesson_3/activity_1/challwaka_pukami_kan.wav') },
   ];
   
   export default audios;
