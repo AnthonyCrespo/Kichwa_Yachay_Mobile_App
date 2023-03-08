4b9de9cdb1e821c532e879e647b3b29e87f69e3e
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-07 23:33:32 -0500

    Added audios L3A1

--- screens/soundsL3A1.jsx
+++ screens/soundsL3A1.jsx
@@ -1,7 +1,17 @@
 const audios = [
-    { name: 'amaruka_killumi_kan.mp3', path: require('../assets/audios/amaruka_killumi_kan.mp3') },
-    { name: 'kuchika_pukami_kan.mp3', path: require('../assets/audios/kuchika_pukami_kan.mp3') },
-    { name: 'missika_yanami_kan.mp3', path: require('../assets/audios/missika_yanami_kan.mp3') },
+    { name: 'amaruka_killumi_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/amaruka_killumi_kan.wav') },
+    { name: 'kuchika_pukami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/kuchika_pukami_kan.wav') },
+    { name: 'missika_yanami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/missika_yanami_kan.wav') },
+
+    { name: 'challwaka_killumi_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/challwaka_killumi_kan.wav') },
+    { name: 'wakraka_pukami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/wakraka_pukami_kan.wav') },
+    { name: 'allkuka_yurakmi_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/allkuka_yurakmi_kan.wav') },
+
+    { name: 'atallpaka_yanami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/atallpaka_yanami_kan.wav') },
+    { name: 'challwaka_pukami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/challwaka_pukami_kan.wav') },
+    
+    { name: 'allkuka_yanami_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/allkuka_yanami_kan.wav') },
+    { name: 'wakraka_yurakmi_kan.mp3', path: require('../assets/audios/unit_1/lesson_3/activity_1/wakraka_yurakmi_kan.wav') }
   ];
   
   export default audios;
