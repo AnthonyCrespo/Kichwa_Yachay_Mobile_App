71667cfcf6ea4732deece088b4ea472c90dfd28b
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-07 23:41:38 -0500

    Fixed L3A3Q1

--- screens/L3A3Q1.jsx
+++ screens/L3A3Q1.jsx
@@ -1,99 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import React,  { useState } from 'react';
-import { StyleSheet, Button,Text, View, TextInput,Image, TouchableOpacity, Linking } from 'react-native';
+import { Alert, StyleSheet, Button,Text, View, TextInput,Image, TouchableOpacity, Linking } from 'react-native';
   
+
+
 const L3A3Q1= ({navigation}) => {
 
     const [leftItems, setLeftItems] = useState(['Perro', 'Cuál', 'Es', 'Negro', 'Blanco', 'Gato']);
     const [rightItems, setRightItems] = useState(['Allku','Maykan', 'Yana', 'Missi', 'Yurak', 'Kan']);
-    const [selectedLeft, setSelectedLeft] = useState(null);
-    const [selectedRight, setSelectedRight] = useState(null);
+    const [selectedLeft, setSelectedLeft] = useState([]);
+    const [selectedRight, setSelectedRight] = useState([]);
+    const [Left, setLeft] = useState([]);
+    const [Right, setRight] = useState([]);
     const [correctPairs, setCorrectPairs] = useState([]);
-    const [showResults, setShowResults] = useState(false);
-    const [isCorrect, setIsCorrect] = useState(false);
     const [incorrectPairs, setIncorrectPairs] = useState([]);
-/*     const [selectedPair, setSelectedPair] = useState({ left: null, right: null });
- */
- /*    const checkPair = (left, right) => {
-      return correctPairs.some((pair) => pair.left === left && pair.right === right);
-    }; */
+  
+
   
     const handlePress = (type, item) => {
       if (type === 'left') {
+        //setLeft([...Left, item])
         setSelectedLeft(item);
       } else {
+        
         setSelectedRight(item);
       }
       if (selectedLeft && type === 'right') {
 
         if (selectedLeft === 'Perro' && item === 'Allku') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
-        } else if (selectedLeft === 'Cuál' && item === 'Maykan') {
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          Alert.alert('Respuesta correcta');
+
+        }
+
+        else if (selectedLeft === 'Cuál' && item === 'Maykan') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          //Alert.alert('Respuesta correcta');
         } 
         else if (selectedLeft === 'Es' && item === 'Kan') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          //Alert.alert('Respuesta correcta');
         }
         else if (selectedLeft === 'Gato' && item === 'Missi') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          //Alert.alert('Respuesta correcta');
         }
         else if (selectedLeft === 'Negro' && item === 'Yana') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          //Alert.alert('Respuesta correcta');
         }
         else if (selectedLeft === 'Blanco' && item === 'Yurak') {
           setCorrectPairs([...correctPairs, { left: selectedLeft, right: item }]);
+          setLeft([...Left, selectedLeft]);
+          setRight([...Right, item]);
+          //Alert.alert('Respuesta correcta');
         }
         else {
-          setIncorrectPairs([...incorrectPairs, { left: selectedLeft, right: item }]);
+            Alert.alert('Respuesta incorrecta');
+            setIncorrectPairs([...incorrectPairs, { left: selectedLeft, right: item }]);
         }
-        setSelectedLeft(null);
-        setSelectedRight(null);
       }
     };
     
-    //--------------
-    let numCorrect = 0;
-    let numIncorrect = 0;
-    //--------------
-    const handleCheckAnswers = () => {
-      const correctAnswers = [
-                              { left: 'Perro', right: 'Allku' }, 
-                              { left: 'Cuál', right: 'Maykan' },
-                              { left: 'Es', right: 'Kan' }, 
-                              { left: 'Gato', right: 'Missi' }, 
-                              { left: 'Negro', right: 'Yana' },
-                              { left: 'Blanco', right: 'Yurak' }, 
-                            ];
-      let isCorrect = true;
-      correctAnswers.forEach(answer=> {
-        if (!correctPairs.some(pair => pair.left === answer.left && pair.right === answer.right)) {
-          isCorrect = false;
-          numIncorrect += 1;
-          incorrectPairs.push(answer);
-        } else{numCorrect += 1;}
-      });
-      setShowResults(true);
-      
-      
-        /* 'Resultados', */
-        isCorrect ? '¡Felicidades, todas tus respuestas son correctas!' : 'Una o más respuestas son incorrectas'
-        [
-          {
-/*             text: 'OK',
- */            onPress: () => setShowResults(false)
-          }
-        ]
-    
-    };
-
     const resetActivity = () => {
       setLeftItems(['Perro', 'Cuál', 'Es', 'Negro', 'Blanco', 'Gato']);
       setRightItems(['Allku','Maykan', 'Yana', 'Missi', 'Yurak', 'Kan']);
-      setSelectedLeft(null);
-      setSelectedRight(null);
+      setSelectedLeft([]);
+      setSelectedRight([]);
+      setLeft([]);
+      setRight([]);
       setCorrectPairs([]);
       setIncorrectPairs([]);
-      setShowResults(false);
     };
 
 
@@ -118,14 +105,14 @@ const L3A3Q1= ({navigation}) => {
         key={`left_${index}`}
         style={[
           styles.buttonSolution,
+          //Left.includes(item)&& styles.alertCorrect,
           isCorrect && styles.correctSolution,
-          isIncorrect && styles.incorrectSolution,
+          //isIncorrect && styles.buttonSolution
+
       ]}
-        onPress={() => {handlePress('left', item);
-                      console.log(selectedLeft)
-          
+        onPress={() => {handlePress('left', item);         
                   }}
-        disabled={ /* correctPairs.find((pair) => pair.left === item) || incorrectPairs.find((pair) => pair.right === item)  &&*/ selectedLeft !== null}
+        disabled={Left.includes(item)}
       >
         <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item}</Text>
       </TouchableOpacity>
@@ -136,19 +123,20 @@ const L3A3Q1= ({navigation}) => {
 <View style={{ flex: 1, flexDirection: 'column', flexWrap: 'wrap' }}>
   {rightItems.map((item, index) => {
      const isCorrect = correctPairs.find((pair) => pair.right === item);
-     const isIncorrect = incorrectPairs.find((pair) => pair.right === item);
+     //const isIncorrect = incorrectPairs.find((pair) => pair.right === item);
      return (
       <TouchableOpacity
         key={`right_${index}`}
         style={[
           styles.buttonSolution,
+          //Right.includes(item)  && styles.alertCorrect,
           isCorrect && styles.correctSolution,
-          isIncorrect && styles.incorrectSolution ,
+
         ]}
         onPress={() => {
           handlePress('right', item);
         }}
-        disabled={/* correctPairs.find((pair) => pair.right === item) && incorrectPairs.find((pair) => pair.left === item) && */ selectedRight !== null}
+        disabled={ Right.includes(item)}
       >
         <Text style={{ fontWeight: 'bold', fontSize: 20 }}>{item}</Text>
       </TouchableOpacity>
@@ -168,14 +156,8 @@ const L3A3Q1= ({navigation}) => {
 
             <View style={styles.buttonContainer}>
               <TouchableOpacity
-                style={styles.checkButton}
-                onPress={() => {
-                  handleCheckAnswers();
-                  alert(numCorrect + " respuestas correctas, " + numIncorrect + " respuestas incorrectas");
-                  resetActivity();
-                }}
               >
-                <Text style={styles.buttonText}>Verificar</Text>
+                <Text style={styles.buttonText}>Continuar</Text>
               </TouchableOpacity>
             </View>
 
@@ -225,7 +207,8 @@ const L3A3Q1= ({navigation}) => {
       alignItems: 'center',
       borderWidth: 1,
       borderColor: 'gray',
-      width: '50%',
+      width: '70%',
+      
       height: 50,
     },
     correctSolution: {
@@ -234,8 +217,12 @@ const L3A3Q1= ({navigation}) => {
     incorrectSolution: {
       backgroundColor:'red',
     }, 
-   /*  Answer: {
-      backgroundColor: '#48d1cc',
-    },  */
+    alertCorrect: { 
+      backgroundColor: 'blue' 
+    },
+    alerIncorrect:{
+      backgroundColor: 'red'
+    }
   });
   export default L3A3Q1
+
