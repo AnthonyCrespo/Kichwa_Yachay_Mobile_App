f9c27e5a231efc550b12be6af0269a433781b3ff
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-03-07 22:42:06 -0500

    Fixed image size L2A2

--- screens/L2A2.jsx
+++ screens/L2A2.jsx
@@ -382,19 +382,19 @@ const styles = StyleSheet.create({
   receivingContainer: {
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    margin: 70
+    marginTop: 30
   },
   itemSeparator: {
     height: 15
   },
   draxListContainer: {
     padding: 5,
-    height: 200
+    height: 80
   },
   receivingZoneContainer: {
     padding: 5,
     height: 100,
-    margin: 70
+    margin: 10
   },
   textStyle: {
     fontSize: 18,
@@ -426,9 +426,10 @@ const styles = StyleSheet.create({
     fontSize: 20
   },
   catImage:{
-    width: 140,
-    height: 100,
-    alignSelf:'center'
+    width: 210,
+    height: 170,
+    alignSelf:'center',
+    marginBottom:20
   },
 
    /* ------- Modal --------- */
