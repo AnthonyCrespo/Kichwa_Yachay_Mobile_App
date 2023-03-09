110b1d6666b4232b46a2474bc85d301ba63c7132
Mateo-Coello <91999743+Mateo-Coello@users.noreply.github.com>
2023-03-09 02:52:31 -0500

    Update L3A2.jsx

--- screens/L3A2.jsx
+++ screens/L3A2.jsx
@@ -57,11 +57,12 @@ const L3A2 = ({navigation}) => {
     setModalVisible(false)
     if (currentQuestionIndex === questions.length-1) {
         navigation.navigate("Result", {puntuation3: Math.round(puntaje), 
-                                      time_taken: segundos, 
-                                      unit:1,
-                                      lesson:3, 
-                                      activity:2,
-                                      subtitle:' '});
+                                     time_taken: segundos,
+                                     unit:1, 
+                                     lesson:1, 
+                                     activity:1,
+                                     subtitle:'Tullkupuna/Wiwakuna',
+                                     subtitle_esp:'Colores/Animales'});
         puntaje = 0;
         currentQuestionIndex = 0
       } else{
