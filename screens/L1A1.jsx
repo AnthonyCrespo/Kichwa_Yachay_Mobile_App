5bacf340a4f7226bb3245a9466d52ef330050053
Mateo-Coello <91999743+Mateo-Coello@users.noreply.github.com>
2023-03-09 02:46:42 -0500

    Update L1A1.jsx

--- screens/L1A1.jsx
+++ screens/L1A1.jsx
@@ -59,7 +59,8 @@ const L1A1 = ({navigation}) => {
                                      unit:1, 
                                      lesson:1, 
                                      activity:1,
-                                     subtitle:'Colores/Tullpukuna'});
+                                     subtitle:'Tullpukuna',
+                                     subtitle_esp:'Colores'});
       puntaje = 0;
     } else{
       setCurrentQuestionIndex(currentQuestionIndex + 1);
